4bb1ef8c5a965eee3753a624c9c5e32a54cc89ec
tfaustyn <78059454+tfaustyn@users.noreply.github.com>
2021-02-07 13:58:34 -0500

    Add files via upload

---
+++ Game/build/classes/game/PhoeniciaCharacter.h
@@ -0,0 +1,72 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+import Test_Sword;
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PhoeniciaCharacter.generated.h"
+#include "../../../../../../../Program Files/Epic Games/UE_4.25/Engine/Plugins/Developer/OneSkyLocalizationService/Source/OneSkyLocalizationService/Private/OneSkyLocalizationServiceResponseTypes.h"
+#include "../../../../../../../Program Files/Epic Games/UE_4.25/Engine/Plugins/Developer/OneSkyLocalizationService/Source/OneSkyLocalizationService/Private/OneSkyLocalizationServiceResponseTypes.h"
+
+class UTextRenderComponent;
+
+/**
+ * This class is the default character for Phoenicia, and it is responsible for all
+ * physical interaction between the player and the world.
+ *
+ * The capsule component (inherited from ACharacter) handles collision with the world
+ * The CharacterMovementComponent (inherited from ACharacter) handles movement of the collision capsule
+ * The Sprite component (inherited from APaperCharacter) handles the visuals
+ */
+UCLASS(config=Game)
+class APhoeniciaCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+
+	/** Side view camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
+	class UCameraComponent* SideViewCameraComponent;
+	
+
+
+	/** Camera boom positioning the camera beside the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UTextRenderComponent* TextComponent;
+	virtual void Tick(float DeltaSeconds) override;
+protected:
+	// The animation to play while running around
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+	class UPaperFlipbook* RunningAnimation;
+
+	// The animation to play while idle (standing still)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	class UPaperFlipbook* IdleAnimation;
+
+	/** Called to choose the correct animation to play based on the character's movement state */
+	void UpdateAnimation();
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	void UpdateCharacter();
+
+	/** Handle touch inputs. */
+	void TouchStarted(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+	/** Handle touch stop event. */
+	void TouchStopped(const ETouchIndex::Type FingerIndex, const FVector Location);
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	// End of APawn interface
+
+public:
+	APhoeniciaCharacter();
+
+	/** Returns SideViewCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+};
