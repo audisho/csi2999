d34ce95ed834edd93d9c5c249d1dd6c9da6423ff
yyjk1031 <78059480+yyjk1031@users.noreply.github.com>
2021-02-23 20:30:17 -0500

    sence and C#code for menu
    here has code for menu and five sence for menu. The oneplayer and twoplayer are just simple sence, if you guys have anything about game, just switch those two.

---
+++ Assets/C#/menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menu : MonoBehaviour
+{
+   
+
+    
+    
+    public void onePlayer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void twoPlayer()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void setting()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void about()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    public void back1p()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void back2p()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+    public void backsetting()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+    public void backabout()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
+    }
+}
